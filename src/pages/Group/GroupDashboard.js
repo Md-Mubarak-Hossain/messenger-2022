b1cf62de42bde5e2609549b9b164de793b1d6364
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-12-04 07:29:25 +0600

    Add Folder

---
+++ src/pages/Group/GroupDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GroupDashboard = () => {
+    return (
+        <div>
+            <h2>group dashboard</h2>
+        </div>
+    );
+};
+
+export default GroupDashboard;
---
+++ src/pages/P2P/P2p.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const P2p = () => {
+    return (
+        <div>
+            <h2>p2p dashboard</h2>
+        </div>
+    );
+};
+
+export default P2p;
